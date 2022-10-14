d2a3e50a7ac13ab2bcb81cbcd5a8e7d75cf5796a
Maksim Shuklin <makshuk@my.com>
2022-10-14 16:57:39 +0300

    fix bug

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,5 +1,6 @@
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import { useNavigate } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 const Page404 = () => {
   const navigate = useNavigate();
@@ -8,24 +9,32 @@ const Page404 = () => {
   };
 
   return (
-    <div>
-      <ErrorMessage />
-      <p style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '24px' }}>
-        Page doesn't exist
-      </p>
-      <div
-        style={{
-          display: 'block',
-          textAlign: 'center',
-          fontWeight: 'bold',
-          fontSize: '24px',
-          marginTop: '30px',
-        }}
-        onClick={onReturn}
-      >
-        Back to main page
+    <>
+      <Helmet>
+        <meta name="description" content="Page doesn't exist" />
+        <title>Page doesn't exist</title>
+      </Helmet>
+      <div>
+        <ErrorMessage />
+        <p
+          style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '24px' }}
+        >
+          Page doesn't exist
+        </p>
+        <div
+          style={{
+            display: 'block',
+            textAlign: 'center',
+            fontWeight: 'bold',
+            fontSize: '24px',
+            marginTop: '30px',
+          }}
+          onClick={onReturn}
+        >
+          Back to main page
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
@@ -1,20 +1,28 @@
 import './singleCharacterLayout.scss';
 import { useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
 
 const SingleCharacterLayout = ({ data }) => {
   const { charName, description, thumbnail } = useParams();
   return (
-    <div className="single-comic">
-      <img
-        src={thumbnail.replaceAll('~', '/')}
-        alt={charName}
-        className="single-comic__char-img"
-      />
-      <div className="single-comic__info">
-        <h2 className="single-comic__name">{charName}</h2>
-        <p className="single-comic__descr">{description}</p>
+    <>
+      <Helmet>
+        <meta name="description" content={`${charName} char description`} />
+        <title>{charName}</title>
+      </Helmet>
+      <div className="single-comic">
+        <img 
+          src={thumbnail.replaceAll('~', '/')}
+          alt={charName}
+          className="single-comic__char-img"
+        />
+        <div className="single-comic__info">
+          <h2 className="single-comic__name">{charName}</h2>
+          <p className="single-comic__descr">{description}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
